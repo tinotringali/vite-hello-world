dc811a3cc2534ac4ed12bc673acc1e55e17b5b47
tinotringali <tringali.tino94@gmail.com>
2023-03-23 01:25:31 +0100

    AppTitle

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default{
+    components:{
+      AppTitle
+    }
+  }
+</script>
+
+<template>
+ <AppTitle></AppTitle>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        data(){
+            return {
+                message:'La mia prima app con Vite!' 
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
